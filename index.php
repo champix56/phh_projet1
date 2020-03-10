e2e08b725961dc0b9289fccfc38a9403f82e6361
champix56 <desorbaix.formation@free.fr>
2020-03-10 12:22:06 +0100

    Premier commit avec une structure minimale html plus css

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+<link rel="stylesheet" href="styles/styles.css">
+
+</head>
+<body>
+    <div id="nav-bar">Une navbar</div>
+    <h1>DEMAT breizhouz</h1>
+    Lorem ipsum dolors <span class="green">sit amet</span>, consectetur adipisicing elit. Hic vero accusantium itaque minima officiis aliquid assumenda delectus eveniet fugiat, accusamus excepturi dolorem expedita at repellat iure unde est explicabo repudiandae.
+</body>
+</html>
